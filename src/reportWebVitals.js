35d74cba9f78481f1140a839f5e8bd1c7ba28e72
yunusbalikci <balikci.yunus06@gmail.com>
2023-11-26 14:07:55 +0300

    first commit

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,9 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
-export default reportWebVitals;
+// reportWebVitals.js
+const reportWebVitals = (value) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(value);
+    }
+  };
+  
+  export default reportWebVitals;
+  
